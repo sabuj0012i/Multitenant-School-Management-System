2fa5343970da07a9fe1b922494444e4a9e5f84bf
Sabuj <sabujhossainraj2000@gmail.com>
2026-03-11 12:41:04 +0600

    Create Teachers page

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Teacher;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class TeacherController extends Controller
+{
+    public function index()
+    {
+        $tenantId = Auth::user()->tenant_id;
+        $teachers = Teacher::where('tenant_id',$tenantId)->get();
+        return Inertia::render('teacher/index',[
+            'tenant_id' => $tenantId,
+            'teachers' => $teachers,
+        ]);
+    }
+
+
+public function store(Request $request)
+{
+    $validated = $request->validate([
+        'first_name' => 'required|string|max50',
+        'last_name' => 'required|string|max50',
+        'subject' => 'required|string|max:50',
+    ]);
+    $validated['tenant_id'] = Auth::user()=>tenant_id;
+    Teacher::create($validated);
+    retrun Redirect::route('teachers.index');
+}
+public function update(Request $reqest, $id)
+{
+    $validated = $request->validate([
+        'frist_name' => 'required|string|max:50',        
+        'last_name' => 'required|string|max:50',        
+        'subject' => 'required|string|max:50',        
+    ])
+    $teacher = Teacher::where('tenant_id',Auth::user()->tenant_id)->findOrFail($id);
+    $teacher -> update($validated);
+    return Redirect::route('teachers.index');
+}
+
+public function destroy($id)
+{
+    $teacher = Teacher::where('tenant_id',Auth::user()->tenant_id)->findOrFail($id);
+    $teacher -> delete();
+    return Redirect::route('teacher.index');
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
+use App\Http\Controllers\TeacherController;
 
 Route::inertia('/', 'welcome', [
     'canRegister' => Features::enabled(Features::registration()),
@@ -9,6 +10,11 @@ Route::inertia('/', 'welcome', [
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::get('/teachers',[TeacherController::class,'index'])->name('teachers.index');
+    Route::post('/teachers',[TeacherController::class,'store'])->name('teachers.store');
+    Route::put('/teachers/{id}',[TeacherController::class,'update'])->name('teachers.update');
+    Route::delete('/teachers/{id}',[TeacherController::class,'destroy'])->name('teachers.destroy');
+
 });
 
 require __DIR__.'/settings.php';
